c8677f1fa425e4a4b56230ac0008e1e6d21208b3
ashutak <ashutak@griddynamics.com>
2019-02-04 00:02:34 +0300

    CrosswordPuzzle - updated (pass all tests)

--- src/main/java/com/ashutak/tasks/hackerrank/CrosswordPuzzle.java
+++ src/main/java/com/ashutak/tasks/hackerrank/CrosswordPuzzle.java
@@ -1,25 +1,8 @@
 package com.ashutak.tasks.hackerrank;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.*;
 
 public class CrosswordPuzzle {
-    static String[] crosswordPuzzle(String[] crossword, String words) {
-        char[][] crosswordArr = new char[crossword.length][crossword.length];
-        for (int i = 0; i < crossword.length; i++) {
-            crosswordArr[i] = crossword[i].toCharArray();
-        }
-        Crossword finalCrossword = new Crossword(crosswordArr);
-        if (!crosswordPuzzle0(finalCrossword, words.split(";"), 0)) {
-            throw new IllegalStateException();
-        }
-        for (int i = 0; i < crosswordArr.length; i++) {
-            crossword[i] = finalCrossword.getRowAsString(i);
-        }
-        return crossword;
-    }
-
     private static class Point {
         final int row, column;
 
@@ -53,8 +36,13 @@ public class CrosswordPuzzle {
             }
         }
 
-        void set(int row, int column, char ch) {
-            crossword[row][column].addLast(ch);
+        boolean set(int row, int column, char ch) {
+            char curChar = crossword[row][column].getLast();
+            if (curChar == ch || curChar == '-') {
+                crossword[row][column].addLast(ch);
+                return true;
+            }
+            return false;
         }
 
         void unsetLast(int row, int column) {
@@ -64,8 +52,8 @@ public class CrosswordPuzzle {
         @Override
         public String toString() {
             StringBuilder sb = new StringBuilder();
-            for (Deque<Character>[] row: crossword) {
-                for (Deque<Character> stack: row){
+            for (Deque<Character>[] row : crossword) {
+                for (Deque<Character> stack : row) {
                     sb.append(stack.getLast());
                 }
             }
@@ -95,113 +83,147 @@ public class CrosswordPuzzle {
         }
     }
 
-    private static boolean crosswordPuzzle0(Crossword crossword, String[] words, int wordIndex) {
+    static String[] crosswordPuzzle(String[] crossword, String words) {
+        char[][] crosswordArr = new char[crossword.length][crossword.length];
+        for (int i = 0; i < crossword.length; i++) {
+            crosswordArr[i] = crossword[i].toCharArray();
+        }
+        Crossword finalCrossword = new Crossword(crosswordArr);
+
+        Map<Integer, List<Point>> wordLength2StartPointsByHorizontal = Collections.unmodifiableMap(
+                createWordLengthIndexInCrosswordByHorizontal(finalCrossword));
+        Map<Integer, List<Point>> wordLength2StartPointsByVertical = Collections.unmodifiableMap(
+                createWordLengthIndexInCrosswordByVertical(finalCrossword));
+
+        if (!crosswordPuzzle0(finalCrossword, words.split(";"), 0, wordLength2StartPointsByHorizontal,
+                wordLength2StartPointsByVertical, new HashSet<>(), new HashSet<>())) {
+            throw new IllegalStateException();
+        }
+        for (int i = 0; i < crosswordArr.length; i++) {
+            crossword[i] = finalCrossword.getRowAsString(i);
+        }
+        return crossword;
+    }
+
+    private static Map<Integer, List<Point>> createWordLengthIndexInCrosswordByHorizontal(Crossword crossword) {
+        Map<Integer, List<Point>> wordLength2StartPoints = new HashMap<>();
+        for (int row = 0; row < crossword.length(); row++) {
+            for (int column = 0; column < crossword.length(); column++) {
+                if (crossword.isFillable(row, column)) {
+                    Point startPoint = new Point(row, column);
+                    column++;
+                    while (column < crossword.length() && crossword.isFillable(row, column)) {
+                        column++;
+                    }
+                    int wordLength = column - startPoint.column;
+                    wordLength2StartPoints.compute(wordLength, (k, v) -> {
+                        if (v == null) {
+                            v = new ArrayList<>();
+                        }
+                        v.add(startPoint);
+                        return v;
+                    });
+                }
+            }
+        }
+        return wordLength2StartPoints;
+    }
+
+    private static Map<Integer, List<Point>> createWordLengthIndexInCrosswordByVertical(Crossword crossword) {
+        Map<Integer, List<Point>> wordLength2StartPoints = new HashMap<>();
+        for (int column = 0; column < crossword.length(); column++) {
+            for (int row = 0; row < crossword.length(); row++) {
+                if (crossword.isFillable(row, column)) {
+                    Point startPoint = new Point(row, column);
+                    row++;
+                    while (row < crossword.length() && crossword.isFillable(row, column)) {
+                        row++;
+                    }
+                    int wordLength = row - startPoint.row;
+                    wordLength2StartPoints.compute(wordLength, (k, v) -> {
+                        if (v == null) {
+                            v = new ArrayList<>();
+                        }
+                        v.add(startPoint);
+                        return v;
+                    });
+                }
+            }
+        }
+        return wordLength2StartPoints;
+    }
+
+    private static boolean crosswordPuzzle0(Crossword crossword, String[] words, int wordIndex,
+                                            Map<Integer, List<Point>> wordLength2StartPointsByHorizontal,
+                                            Map<Integer, List<Point>> wordLength2StartPointsByVertical,
+                                            Set<Point> usedHorizintalPositions,
+                                            Set<Point> usedVerticalPositions) {
         if (wordIndex >= words.length) {
             return true;
         }
 
         String word = words[wordIndex];
-        char[] wordArr = word.toCharArray();
-        System.out.println(word);
 
-        Point p = findHorizontalRow(crossword, word, new Point(0,0));
-        while (p != null) {
-//            System.out.println(p);
-            fillHorizontalRow(crossword, wordArr, p);
-            if (crosswordPuzzle0(crossword, words, wordIndex + 1)) {
-                return true;
+        List<Point> points = wordLength2StartPointsByHorizontal.get(word.length());
+        if (points != null) {
+            for (Point p : points) {
+                if (!usedHorizintalPositions.contains(p) && fillWordInCrosswordHorisontally(crossword, word, p)) {
+                    usedHorizintalPositions.add(p);
+                    if (crosswordPuzzle0(crossword, words, wordIndex + 1,
+                            wordLength2StartPointsByHorizontal, wordLength2StartPointsByVertical,
+                            usedHorizintalPositions, usedVerticalPositions)) {
+                        return true;
+                    }
+                    usedHorizintalPositions.remove(p);
+                    for (int letterIdx = 0; letterIdx < word.length(); letterIdx++) {
+                        crossword.unsetLast(p.row, p.column + letterIdx);
+                    }
+                }
             }
-            for (int letterIdx = 0; letterIdx < wordArr.length; letterIdx++) {
-                crossword.unsetLast(p.row, p.column + letterIdx);
-            }
-            p = findHorizontalRow(crossword, word, new Point(p.row, p.column + 1));
         }
-
-        p = findVerticalColumn(crossword, word, new Point(0,0));
-        while (p != null) {
-            fillVerticalColumn(crossword, wordArr, p);
-            if (crosswordPuzzle0(crossword, words, wordIndex + 1)) {
-                return true;
+        points = wordLength2StartPointsByVertical.get(word.length());
+        if (points != null) {
+            for (Point p : points) {
+                if (!usedVerticalPositions.contains(p) && fillWordInCrosswordVertically(crossword, word, p)) {
+                    usedVerticalPositions.add(p);
+                    if (crosswordPuzzle0(crossword, words, wordIndex + 1,
+                            wordLength2StartPointsByHorizontal, wordLength2StartPointsByVertical,
+                            usedHorizintalPositions, usedVerticalPositions)) {
+                        return true;
+                    }
+                    usedVerticalPositions.remove(p);
+                    for (int letterIdx = 0; letterIdx < word.length(); letterIdx++) {
+                        crossword.unsetLast(p.row + letterIdx, p.column);
+                    }
+                }
             }
-            for (int letterIdx = 0; letterIdx < wordArr.length; letterIdx++) {
-                crossword.unsetLast(p.row + letterIdx, p.column);
-            }
-            p = findVerticalColumn(crossword, word, new Point(p.row + 1, p.column));
         }
-
         return false;
     }
 
-    private static void fillVerticalColumn(Crossword crossword, char[] word, Point p) {
-        for (int letterIdx = 0; letterIdx < word.length; letterIdx++) {
-            crossword.set(p.row + letterIdx, p.column, word[letterIdx]);
-        }
-    }
-
-    private static Point findVerticalColumn(Crossword crossword, String word, Point fromPoint) {
-        for (int columnIdx = fromPoint.column; columnIdx < crossword.length(); columnIdx++) {
-            int rowIdx = 0;
-
-            if (columnIdx == fromPoint.column)
-                rowIdx = fromPoint.row;
-
-            for (; rowIdx < crossword.length() - word.length() + 1; rowIdx++) {
-                if (crossword.isFillable(rowIdx, columnIdx)) {
-                    Point p = new Point(rowIdx, columnIdx);
-                    if (isOkeyVerticalPoint(crossword, word, p)) {
-                        return p;
-                    }
-                }
-            }
-
-        }
-        return null;
-    }
-
-    private static void fillHorizontalRow(Crossword crossword, char[] word, Point p) {
-        for (int letterIdx = 0; letterIdx < word.length; letterIdx++) {
-            crossword.set(p.row, p.column + letterIdx, word[letterIdx]);
-        }
-    }
-
-    private static Point findHorizontalRow(Crossword crossword, String word, Point fromPoint) {
-        for (int rowIdx = fromPoint.row; rowIdx < crossword.length(); rowIdx++) {
-            int columnIdx = 0;
-
-            if (rowIdx == fromPoint.column)
-                columnIdx = fromPoint.column;
-
-            for (; columnIdx < crossword.length() - word.length() + 1; columnIdx++) {
-                if (crossword.isFillable(rowIdx, columnIdx)) {
-                    Point p = new Point(rowIdx, columnIdx);
-                    if (isOkeyHorizontalPoint(crossword, word, p)) {
-                        return p;
-                    }
-                }
-            }
-        }
-        return null;
-    }
-
-    private static boolean isOkeyHorizontalPoint(Crossword crossword, String word, Point p) {
+    private static boolean fillWordInCrosswordHorisontally(Crossword crossword, String word, Point p) {
         for (int letterIdx = 0; letterIdx < word.length(); letterIdx++) {
-            char crosswordChar = crossword.get(p.row,p.column + letterIdx);
-            boolean isValid = crosswordChar == '-' || crosswordChar == word.charAt(letterIdx);
-            if (!isValid) {
+            if (!crossword.set(p.row, p.column + letterIdx, word.charAt(letterIdx))) {
+                letterIdx--;
+                for (; letterIdx >= 0; letterIdx--) {
+                    crossword.unsetLast(p.row, p.column + letterIdx);
+                }
                 return false;
             }
         }
-        return p.column + word.length() >= crossword.length() || !crossword.isFillable(p.row, p.column + word.length());
+        return true;
     }
 
-    private static boolean isOkeyVerticalPoint(Crossword crossword, String word, Point p) {
+    private static boolean fillWordInCrosswordVertically(Crossword crossword, String word, Point p) {
         for (int letterIdx = 0; letterIdx < word.length(); letterIdx++) {
-            char crosswordChar = crossword.get(p.row + letterIdx,p.column);
-            boolean isValid = crosswordChar == '-' || crosswordChar == word.charAt(letterIdx);
-            if (!isValid) {
+            if (!crossword.set(p.row + letterIdx, p.column, word.charAt(letterIdx))) {
+                letterIdx--;
+                for (; letterIdx >= 0; letterIdx--) {
+                    crossword.unsetLast(p.row + letterIdx, p.column);
+                }
                 return false;
             }
         }
-        return p.row + word.length() >= crossword.length() || !crossword.isFillable(p.row + word.length(), p.column);
+        return true;
     }
 }
